f90a614940f4a8f102dc5612960aa21ff510d9e8
Vikas Chaturvedi <vc7068318009@gmail.com>
2024-04-08 19:39:05 +0530

    done3

--- models/CartModel.js
+++ models/CartModel.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
 
 const cartSchema = new mongoose.Schema({
-  userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-  productId: { type: mongoose.Schema.Types.ObjectId, ref: 'Product', required: true },
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true,default: null },
+  productId: { type: mongoose.Schema.Types.ObjectId, ref: 'Product', required: true,default: null },
   quantity: { type: Number, default: 1 },
 });
 
